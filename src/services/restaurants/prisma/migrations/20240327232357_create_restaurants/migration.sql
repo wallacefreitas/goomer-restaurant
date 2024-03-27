cd1c188cf514a3abfd67adcea3604a3720fc8191
Wallace de Freitas <wallace.freitas.b@gmail.com>
2024-03-27 20:58:48 -0300

    feat: created restaurant model + migrations

---
+++ src/services/restaurants/prisma/migrations/20240327232357_create_restaurants/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "restaurants" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "image" TEXT NOT NULL,
+
+    CONSTRAINT "restaurants_pkey" PRIMARY KEY ("id")
+);
