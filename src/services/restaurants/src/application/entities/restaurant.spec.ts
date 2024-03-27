12ee254c92c74088ee39c243eece0607578af961
Wallace de Freitas <wallace.freitas.b@gmail.com>
2024-03-26 21:20:47 -0300

    test: created test to restaurant entity

---
+++ src/services/restaurants/src/application/entities/restaurant.spec.ts
@@ -0,0 +1,36 @@
+import { it, describe, expect } from "vitest"
+import { Restaurant } from "./restaurant"
+
+describe('restaurant class', () => {
+  it("allow created an restaurant", () => {
+    const data = {
+      name: "Restaurant XPTO",
+      address: "Street of Rages",
+      image: "http://localhost:1000/images/123456789.png"
+    }
+
+    const restaurant = new Restaurant(data)
+
+    expect(restaurant).toBeInstanceOf(Restaurant)
+  })
+
+  it("not allow created an restaurant without name", () => {
+    const data = {
+      name: "",
+      address: "Street of Rages",
+      image: "http://localhost:1000/images/123456789.png"
+    }
+
+    expect(() => new Restaurant(data)).toThrowError()
+  })
+
+  it("allow created an restaurant without image", () => {
+    const data = {
+      name: "",
+      address: "Street of Rages",
+      image: ""
+    }
+
+    expect(() => new Restaurant(data)).toThrowError()
+  })
+})
