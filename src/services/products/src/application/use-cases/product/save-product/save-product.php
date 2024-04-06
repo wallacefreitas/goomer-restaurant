d039cb7acc91c1effc39924d0280d569f70f8a30
Wallace de Freitas <wallace.freitas.b@gmail.com>
2024-04-06 17:46:13 -0300

    chore: removed breakline

--- src/services/products/src/application/use-cases/product/save-product/save-product.php
+++ src/services/products/src/application/use-cases/product/save-product/save-product.php
@@ -1,16 +1,15 @@
 <?php
+  include_once('./src/application/entities/product.php');
+  include_once('./src/application/repositories/products-repository.php');
 
-include_once('./src/application/entities/product.php');
-include_once('./src/application/repositories/products-repository.php');
+  class SaveProduct {
+    private $productRepository;
 
-class SaveProduct {
-  private $productRepository;
+    function __construct(ProductsRepository $productRepository) {
+      $this->productRepository = $productRepository;
+    }
 
-  function __construct(ProductsRepository $productRepository) {
-    $this->productRepository = $productRepository;
+    function execute(Product $product) {
+      return $this->productRepository->save($product);
+    }
   }
-
-  function execute(Product $product) {
-    return $this->productRepository->save($product);
-  }
-}
