82ceb1e2f7b4eeed4fd35d4791c7db4ddce581fa
Wallace de Freitas <wallace.freitas.b@gmail.com>
2024-04-06 16:03:36 -0300

    feat: created router service

---
+++ src/services/products/src/infra/services/router.php
@@ -0,0 +1,27 @@
+<?php
+  class Router {
+    protected $routes = [];
+
+    public function addRoute(string $method, string $url, closure $target) {
+      $this->routes[$method][$url] = $target;
+    }
+
+    public function matchRoute() {
+      $method = $_SERVER['REQUEST_METHOD'];
+      $url = $_SERVER['REQUEST_URI'];
+
+      if (isset($this->routes[$method])) {
+        foreach ($this->routes[$method] as $routeUrl => $target) {
+          $pattern = preg_replace('/\/:([^\/]+)/', '/(?P<$1>[^/]+)', $routeUrl);
+
+          if (preg_match('#^' . $pattern . '$#', $url, $matches)) {
+              $params = array_filter($matches, 'is_string', ARRAY_FILTER_USE_KEY); // Only keep named subpattern matches
+              call_user_func_array($target, $params);
+              return;
+          }
+        }
+      }
+
+      throw new Exception('Route not found');
+    }
+  }
