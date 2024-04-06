a270de2696afc315e1558734cfb186ce90b24197
Wallace de Freitas <wallace.freitas.b@gmail.com>
2024-04-06 16:24:08 -0300

    feat: created interface repository to product

---
+++ src/services/products/src/application/repositories/products-repository.php
@@ -0,0 +1,11 @@
+<?php
+
+include_once('./src/application/entities/product.php');
+
+interface ProductsRepository {
+  public function create(Product $product): void;
+  public function findAll(): array | Product;
+  public function findUnique(string $id);
+  public function save(Product $product): bool;
+  public function remove(string $id): bool;
+}
