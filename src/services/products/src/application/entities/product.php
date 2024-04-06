baaa381f6b7921704025a19b13c594bcd98bfc5d
Wallace de Freitas <wallace.freitas.b@gmail.com>
2024-04-06 17:45:29 -0300

    chore: added space in if sentence

--- src/services/products/src/application/entities/product.php
+++ src/services/products/src/application/entities/product.php
@@ -1,47 +1,46 @@
 <?php
+  enum Categories: string {
+    case Doces = '1';
+    case Salgados = '2';
+    case Sucos = '3';
+  }
 
-enum Categories: string {
-  case Doces = '1';
-  case Salgados = '2';
-  case Sucos = '3';
-}
+  class Product {
+    public $props;
 
-class Product {
-  public $props;
+    function __construct($props) {
+      if (!isset($props->id)) {
+        $props->id = uniqid();
+      }
 
-  function __construct($props) {
-    if(!isset($props->id)) {
-      $props->id = uniqid();
+      try {
+        $this->props = $props;
+      } catch (Exception $e) {
+        echo $e->getMessage();
+      }
     }
 
-    try {
-      $this->props = $props;
-    } catch (Exception $e) {
-      echo $e->getMessage();
+    function getName() {
+      return $this->props->name;
+    }
+
+    function setName(string $name) {
+      $this->props->name = $name;
+    }
+
+    function getPrice() {
+      return $this->props->price;
+    }
+
+    function setPrice(float $price) {
+      $this->props->price = $price;
+    }
+
+    function getCategory() {
+      return $this->props->category;
+    }
+
+    function setCategory(Categories $category) {
+      $this->props->category = $category;
     }
   }
-
-  function getName() {
-    return $this->props->name;
-  }
-
-  function setName(string $name) {
-    $this->props->name = $name;
-  }
-
-  function getPrice() {
-    return $this->props->price;
-  }
-
-  function setPrice(float $price) {
-    $this->props->price = $price;
-  }
-
-  function getCategory() {
-    return $this->props->category;
-  }
-
-  function setCategory(Categories $category) {
-    $this->props->category = $category;
-  }
-}
