e5d4431fa4aa7fa3fd5b4243a28318bb0a3d9ce2
Wallace de Freitas <wallace.freitas.b@gmail.com>
2024-04-06 15:02:05 -0300

    fix: changed url added version path

--- src/services/restaurants/src/routes.ts
+++ src/services/restaurants/src/routes.ts
@@ -4,23 +4,23 @@ import * as restaurantUseCase from "./application/use-cases/restaurant"
 import { Restaurant } from "./application/entities/restaurant";
 
 export async function routes(server: FastifyInstance) {
-  server.post('/restaurant', (request: FastifyRequest<{Body: Restaurant}>, response: FastifyReply) => {
+  server.post('/v1/restaurant', (request: FastifyRequest<{Body: Restaurant}>, response: FastifyReply) => {
     return restaurantUseCase.createRestaurantController.handle(request, response)
   })
 
-  server.get('/restaurants', (request: FastifyRequest, response: FastifyReply) => {
+  server.get('/v1/restaurants', (request: FastifyRequest, response: FastifyReply) => {
     return restaurantUseCase.findAllRestaurantsController.handle(request, response)
   })
 
-  server.get('/restaurant/:id', (request: FastifyRequest<{Params: { id: string }}>, response: FastifyReply) => {
+  server.get('/v1/restaurant/:id', (request: FastifyRequest<{Params: { id: string }}>, response: FastifyReply) => {
     return restaurantUseCase.findUniqueRestaurantController.handle(request, response)
   })
 
-  server.delete('/restaurant/:id', (request: FastifyRequest<{Params: { id: string }}>, response: FastifyReply) => {
+  server.delete('/v1/restaurant/:id', (request: FastifyRequest<{Params: { id: string }}>, response: FastifyReply) => {
     return restaurantUseCase.removeRestaurantController.handle(request, response)
   })
 
-  server.put('/restaurant/:id', (request: FastifyRequest<{Params: { id: string }, Body: Restaurant}>, response: FastifyReply) => {
+  server.put('/v1/restaurant/:id', (request: FastifyRequest<{Params: { id: string }, Body: Restaurant}>, response: FastifyReply) => {
     return restaurantUseCase.saveRestaurantController.handle(request, response)
   })
 }
