19b8d0fe5d15e568f4e52c2ada536b2bbba7f2a9
Wallace de Freitas <wallace.freitas.b@gmail.com>
2024-04-06 16:23:19 -0300

    feat: created route file

---
+++ src/services/products/routes.php
@@ -0,0 +1,59 @@
+<?php
+  include_once('./src/infra/services/router.php');
+  include_once('./src/application/entities/product.php');
+  
+  include_once('./src/infra/repositories/in-memory/in-memory-product-repository.php');
+
+  include_once('./src/infra/http/controller/product/find-all-products-controller.php');
+  include_once('./src/infra/http/controller/product/find-unique-product-controller.php');
+  include_once('./src/infra/http/controller/product/create-product-controller.php');
+  include_once('./src/infra/http/controller/product/save-product-controller.php');
+  include_once('./src/infra/http/controller/product/remove-product-controller.php');
+
+  include_once('./src/application/use-cases/product/create-product/create-product.php');
+  include_once('./src/application/use-cases/product/save-product/save-product.php');
+  include_once('./src/application/use-cases/product/find-all-products/find-all-products.php');
+  include_once('./src/application/use-cases/product/find-unique-product/find-unique-product.php');
+  include_once('./src/application/use-cases/product/remove-product/remove-product.php');
+ 
+  function routes() {
+    $router = new Router();
+    $inMemoryProductRepository = new InMemoryProductRepository();
+
+    $router->addRoute('GET', '/products', function() use($inMemoryProductRepository) {
+      $findAllProducts = new FindAllProducts($inMemoryProductRepository);
+      $findAllProductsController = new FindAllProductsController($findAllProducts);
+
+      return $findAllProductsController->handle();
+    });
+
+    $router->addRoute('GET', '/product/:id', function($id) use($inMemoryProductRepository) {
+      $findUniqueProduct = new FindUniqueProduct($inMemoryProductRepository);
+      $findUniqueProductController = new FindUniqueProductController($findUniqueProduct);
+
+      return $findUniqueProductController->handle($id);
+    });
+
+    $router->addRoute('POST', '/product', function($id) use($inMemoryProductRepository) {
+      $createProduct = new CreateProduct($inMemoryProductRepository);
+      $createProductController = new CreateProductController($createProduct);
+
+      return $createProductController->handle($id);
+    });
+
+    $router->addRoute('PUT', '/product/:id', function($id) use($inMemoryProductRepository) {
+      $saveProduct = new SaveProduct($inMemoryProductRepository);
+      $saveProductController = new SaveProductController($saveProduct);
+
+      return $saveProductController->handle($id);
+    });
+
+    $router->addRoute('DELETE', '/product/:id', function($id) use($inMemoryProductRepository) {
+      $removeProduct = new RemoveProduct($inMemoryProductRepository);
+      $removeProductController = new RemoveProductController($removeProduct);
+
+      return $removeProductController->handle($id);
+    });
+
+    $router->matchRoute();
+  }
