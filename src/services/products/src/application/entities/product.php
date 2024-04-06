f580468f0001b4f3ef70503405f5a92cd2cce30f
Wallace de Freitas <wallace.freitas.b@gmail.com>
2024-04-06 10:09:31 -0300

    feat: created product entity

---
+++ src/services/products/src/application/entities/product.php
@@ -0,0 +1,47 @@
+<?php
+
+enum Categories: string {
+  case Doces = '1';
+  case Salgados = '2';
+  case Sucos = '3';
+}
+
+class Product {
+  public $props;
+
+  function __construct($props) {
+    if(!isset($props->id)) {
+      $props->id = uniqid();
+    }
+
+    try {
+      $this->props = $props;
+    } catch (Exception $e) {
+      echo $e->getMessage();
+    }
+  }
+
+  function getName() {
+    return $this->props->name;
+  }
+
+  function setName(string $name) {
+    $this->props->name = $name;
+  }
+
+  function getPrice() {
+    return $this->props->price;
+  }
+
+  function setPrice(float $price) {
+    $this->props->price = $price;
+  }
+
+  function getCategory() {
+    return $this->props->category;
+  }
+
+  function setCategory(Categories $category) {
+    $this->props->category = $category;
+  }
+}
